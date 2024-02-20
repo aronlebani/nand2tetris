a15e01f0f2f4f69d257805862bc7eec8cce59fb9
Aron Lebani <aron@lebani.dev>
2024-02-20 21:00:44 +1100

    Finish project 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,67 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+
+@KBD
+D=M
+
+@FILL
+D;JGT
+
+@CLEAR
+D;JEQ
+
+(FILL)
+	@SCREEN
+	D=A // D = 16384
+
+	@i
+	M=D	// i = 16384
+
+	(LOOP-FILL)
+		@i
+		A=M
+		M=-1  // -1 = 0b1111111111111111
+		
+		@i
+		M=M+1 // i = i + 1
+		
+		@i
+		D=M  // D = i
+
+		@24575
+		D=D-A // i = i - 24575
+		
+		@LOOP-FILL
+		D;JLE
+	
+	@LOOP
+	0;JMP
+
+(CLEAR)
+	@SCREEN
+	D=A // D = 16384
+
+	@i
+	M=D	// i = 16384
+
+	(LOOP-CLEAR)
+		@i
+		A=M
+		M=0
+		
+		@i
+		M=M+1 // i = i + 1
+		
+		@i
+		D=M  // D = i
+
+		@24575
+		D=D-A // i = i - 24575
+		
+		@LOOP-CLEAR
+		D;JLE
+
+	@LOOP
+	0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,32 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// r2 = 0
+@R2
+M=0
+
+(LOOP)
+
+// if r1 <= 0, break
+@R1
+D=M
+@END
+D;JLE
+
+// r2 = r2 + r0
+@R0
+D=M
+@R2
+M=D+M
+
+// r1 = r1 - 1
+@R1
+M=M-1
+
+@LOOP
+0;JMP
+
+(END)
+
+@END
+0;JMP
